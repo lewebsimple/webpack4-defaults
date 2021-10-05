8c3f09adbc47a1f121d06fe20cd26e094f195127
Pascal Martineau <pascal@lewebsimple.ca>
2021-10-04 22:37:50 -0400

    Resolve .js / .ts / .vue extensions

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,27 @@
-const { resolve } = require('path');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { resolve } = require("path");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
-const mode = process.argv.includes('production') ? 'production' : 'development';
+const mode = process.argv.includes("production") ? "production" : "development";
 
 module.exports = {
-
   mode,
 
   module: {
     rules: [
-
       // JavaScript
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: ['@babel/preset-env'],
-          plugins: ['@babel/plugin-transform-runtime'],
+          presets: ["@babel/preset-env"],
+          plugins: ["@babel/plugin-transform-runtime"],
         },
       },
 
@@ -31,16 +30,16 @@ module.exports = {
         test: /\.tsx?$/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-transform-runtime'],
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-transform-runtime"],
             },
           },
-          {            
-            loader: 'ts-loader',
+          {
+            loader: "ts-loader",
             options: { appendTsxSuffixTo: [/\.vue$/] },
-          }
+          },
         ],
         exclude: /node_modules/,
       },
@@ -49,13 +48,13 @@ module.exports = {
       {
         test: /\.mjs$/,
         include: /node_modules/,
-        type: "javascript/auto"
+        type: "javascript/auto",
       },
 
       // Vue.js
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: "vue-loader",
       },
 
       // CSS / SCSS
@@ -64,18 +63,18 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: { sourceMap: false },
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: { sourceMap: false },
           },
           {
-            loader: 'resolve-url-loader',
+            loader: "resolve-url-loader",
           },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: { sourceMap: true },
           },
         ],
@@ -84,15 +83,15 @@ module.exports = {
       // Fonts
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        loader: 'file-loader',
-        options: { name: 'fonts/[name].[ext]' },
+        loader: "file-loader",
+        options: { name: "fonts/[name].[ext]" },
       },
 
       // Images
       {
         test: /\.(png|jpg|jpeg|gif)$/i,
-        loader: 'file-loader',
-        options: { name: 'images/[name].[ext]' },
+        loader: "file-loader",
+        options: { name: "images/[name].[ext]" },
       },
 
       // SVG
@@ -101,67 +100,66 @@ module.exports = {
         oneOf: [
           {
             resourceQuery: /inline/,
-            use: [
-              'babel-loader',
-              'vue-svg-loader',
-            ],
+            use: ["babel-loader", "vue-svg-loader"],
           },
           {
-            loader: 'file-loader',
-            options: { name: 'images/[name].[ext]' },
+            loader: "file-loader",
+            options: { name: "images/[name].[ext]" },
           },
         ],
       },
-
     ],
   },
 
   plugins: [
-
     // Browsersync
-    ...(process.env.PROXY_HOST ? [new BrowserSyncPlugin({
-      host: 'localhost',
-      port: process.env.PROXY_PORT || 3000,
-      proxy: process.env.PROXY_HOST,
-      notify: false,
-      files: ['./**/*.php'],
-    })] : []),
+    ...(process.env.PROXY_HOST
+      ? [
+          new BrowserSyncPlugin({
+            host: "localhost",
+            port: process.env.PROXY_PORT || 3000,
+            proxy: process.env.PROXY_HOST,
+            notify: false,
+            files: ["./**/*.php"],
+          }),
+        ]
+      : []),
 
     // Clean assets directory
-    ...(mode === 'production' ? [new CleanWebpackPlugin()] : []),
+    ...(mode === "production" ? [new CleanWebpackPlugin()] : []),
 
     // Extract styles to a single CSS file
-    new MiniCssExtractPlugin({ filename: 'css/[name].css', }),
+    new MiniCssExtractPlugin({ filename: "css/[name].css" }),
 
     // Vue.js
     new VueLoaderPlugin(),
-
   ],
 
   optimization: {
     // Minimize JS / CSS
-    minimizer: [
-      new OptimizeCssAssetsPlugin(),
-      new TerserPlugin({ extractComments: (astNode, comment) => false }),
-    ],
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin({ extractComments: (astNode, comment) => false })],
     // Split vendor chunk
     splitChunks: {
       cacheGroups: {
         vendor: {
-          test:  /node_modules\/(.*)\.js/,
-          name: 'vendor',
-          chunks: 'initial',
+          test: /node_modules\/(.*)\.js/,
+          name: "vendor",
+          chunks: "initial",
         },
       },
     },
   },
 
-  // Resolve loaders relative to webpack4-defaults
-  resolveLoader: {
-    modules: ['node_modules', resolve(__dirname, 'node_modules')],
+  resolve: {
+    extensions: [".js", ".ts", ".vue"],
   },
 
-  devtool: mode === 'development' ? 'source-map' : false,
+  // Resolve loaders relative to webpack4-defaults
+  resolveLoader: {
+    modules: ["node_modules", resolve(__dirname, "node_modules")],
+  },
+
+  devtool: mode === "development" ? "source-map" : false,
 
   performance: { hints: false, maxAssetSize: 8388608 },
 
@@ -171,5 +169,4 @@ module.exports = {
     errors: true,
     warnings: true,
   },
-
 };
